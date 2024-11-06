868da49129482f3ebecaffd7b56f80170cecf8ef
O.Gogol <o.gogol91@gmail.com>
2024-11-06 15:24:14 +0000

    scripts added

---
+++ src/main/java/ru/netology/hw_jacoco/statistic/StatisticsService.java
@@ -0,0 +1,13 @@
+package ru.netology.hw_jacoco.statistic;
+
+public class StatisticsService {
+    public long findMax(long[] incomes) {
+        long currentMax = incomes[0];
+        for (long income : incomes) {
+            if (currentMax < income) {
+                currentMax = income;
+            }
+        }
+        return currentMax;
+    }
+}
